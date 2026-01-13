7b75fac8d51d1bdc367d3d6702ceba9177d6cc83
ArmoredCyrax <92490863+ArmoredCyrax@users.noreply.github.com>
2026-01-13 15:26:01 -0800

    Add files via upload

---
+++ doubleInt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    std::cout << "Enter an integer, and I will double it:";
+
+    int i;
+
+    cin >> i;
+
+    cout << 2*i;
+
+    return 0;
+
+}
