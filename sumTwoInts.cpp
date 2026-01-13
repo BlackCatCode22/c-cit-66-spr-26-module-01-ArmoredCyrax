c87188f1737fefe6de4933901ff8bc78c91fc71c
ArmoredCyrax <92490863+ArmoredCyrax@users.noreply.github.com>
2026-01-13 15:50:56 -0800

    Add files via upload

---
+++ sumTwoInts.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    std::cout << "Enter two integers, and I will give you the sum: ";
+
+    int i;
+
+    int j;
+
+    cin >> i;
+
+    std::cout << "Enter the second, and I will give you the sum: ";
+
+    cin >> j;
+
+    cout << i+j;
+
+    return 0;
+}
