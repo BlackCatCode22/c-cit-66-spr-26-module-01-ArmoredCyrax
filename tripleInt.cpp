d515fb262ab1167086cdf3159cccb0a1eecd0f58
ArmoredCyrax <92490863+ArmoredCyrax@users.noreply.github.com>
2026-01-13 15:44:39 -0800

    Add files via upload

---
+++ tripleInt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    std::cout << "Enter an integer, and I will triple it: ";
+
+    int i;
+
+    cin >> i;
+
+    cout << i*3;
+
+    return 0;
+}
