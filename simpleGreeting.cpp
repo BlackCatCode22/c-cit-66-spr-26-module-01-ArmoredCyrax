07980104aff136cc703554e3abdb135d2d7c6475
ArmoredCyrax <92490863+ArmoredCyrax@users.noreply.github.com>
2026-01-13 16:05:54 -0800

    Add files via upload

---
+++ simpleGreeting.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name;
+
+    std::cout << "Hello, what's your name?";
+
+    std::cin >> name;
+
+    std::cout << "Hello "<<name<<"!";
+
+    return 0;
+}
