d18fa56a2f3ad50016523aa17c59cd710d3b718b
ArmoredCyrax <92490863+ArmoredCyrax@users.noreply.github.com>
2026-01-15 03:57:30 -0800

    Update doubleInt.cpp

--- doubleInt.cpp
+++ doubleInt.cpp
@@ -4,14 +4,11 @@ using namespace std;
 
 int main() {
 
-    std::cout << "Enter an integer, and I will double it:";
+    cout << "Enter an integer, and I will double it:";
 
     int i;
 
     cin >> i;
 
     cout << 2*i;
-
-    return 0;
-
 }
